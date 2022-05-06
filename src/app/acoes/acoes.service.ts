20c2a16e3b1090b535a8f6cfbbecc6cd69292099
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2022-05-06 10:21:12 -0300

    Refatorando serviço de retorno de dados da api

--- src/app/acoes/acoes.service.ts
+++ src/app/acoes/acoes.service.ts
@@ -1,5 +1,8 @@
+import { Acao, Acoes, AcoesAPI } from './modelo/acoes';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, pluck, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +10,25 @@ import { HttpClient } from '@angular/common/http';
 export class AcoesService {
   constructor(private httpClient: HttpClient) {}
 
-  getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes');
+  getAcoes(): Observable<Acoes> {
+    return this.httpClient.get<AcoesAPI>('http://localhost:3000/acoes').pipe(
+      tap((value) => {
+        console.log(value);
+      }),
+      pluck('payload'),
+      map((acoes) => acoes.sort((a, b) => this.ordenaPorCodigo(a, b)))
+    );
+  }
+
+  private ordenaPorCodigo(a: Acao, b: Acao) {
+    if (a.codigo > b.codigo) {
+      return 1;
+    }
+
+    if (a.codigo < b.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }
